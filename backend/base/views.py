19fa11bc4b38a070d765229c8ffc7e61473d387c
dheerut04 <dheerajtalluru@gmail.com>
2025-08-09 19:22:27 +0530

    backend: Insall Django nd setup django.
    	* Adds: Virtual Env called as Scrap :)
    
    Signed-off-by: dheerut04 <dheerajtalluru@gmail.com>

---
+++ backend/base/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
+
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload/',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+    ]
+    return Response(routes)
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
